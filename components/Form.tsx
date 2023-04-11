4eb912ad735fe50aca738b6dcdebbb236c7f1859
Martin Yeung <luckylucky2010201@yahoo.com.hk>
2023-04-11 12:57:45 +0800

    20230411 build 2

--- components/Form.tsx
+++ components/Form.tsx
@@ -32,13 +32,66 @@ const Form = () => {
     setIsLoading(false);
   };
 
+  const [message, setMessage] = useState<null | string | undefined>(null);
+  const [isSuccess, setIsSuccess] = useState(false);
+  //const stripe = useStripe();
+
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent?.status) {
+        case "succeeded":
+          setIsSuccess(true);
+          setMessage("Your payment was successfull!");
+          break;
+        case "processing":
+          setMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe]);
+
   return (
-    <form onSubmit={handleSubmit}>
-      <PaymentElement />
-      <button className={styles.mainButton} disabled={isLoading || !stripe || !elements}>
-        <span>{isLoading ? "Loading..." : "Pay now"}</span>
-      </button>
-    </form>
+    <>
+      {message ? (
+        <>
+          {isSuccess && (
+            <a
+              href="https://testnets.opensea.io/assets/mumbai/YOUR_EDITION_ADDRESS/0"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Check out your NFT
+            </a>
+          )}
+          <h1>{message}</h1>
+        </>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <PaymentElement />
+          <button className={styles.mainButton} disabled={isLoading || !stripe || !elements}>
+            <span>{isLoading ? "Loading..." : "Pay now"}</span>
+          </button>
+        </form>
+      )}
+    </>
   );
 };
 
